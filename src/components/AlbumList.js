addfff8f780043bc6a1c56c11ca043398b9a7ba7
david ladowitz <davidladowitz@davids-MacBook-Pro.local>
2016-11-19 18:58:09 -0800

    Add http get request

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Homework</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  state = {
+    albums: [{ title: 'None' }]
+  };
+
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+    console.log('componentWillMount - State', this.state);
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => {
+      console.log('Http Response: ', response);
+      this.setState({ albums: response.data });
+      console.log('After http get - State', this.state);
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        {this.state.albums.map((album) => <Text key={album.title}>{album.title}</Text>)}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
