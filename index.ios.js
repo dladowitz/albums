0a2111485c60c15dd4cc1b86431bc938b6d10c25
david ladowitz <davidladowitz@davids-MacBook-Pro.local>
2016-11-19 16:32:58 -0800

    Add albumlist

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,15 @@
 // Import a library to help create a Component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header.js';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header.js';
+import AlbumList from './src/components/AlbumList';
 
 // Create a Component
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'InstaAlbums'} />
+    <AlbumList />
+  </View>
 );
 
 
